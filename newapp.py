1e73b84c9b3c970369db5a86f064561a8f18b805
tvvyas <tarun20vyas@gmail.com>
2024-08-16 08:51:00 +0530

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -1,7 +1,19 @@
 import streamlit as st
 import pandas as pd
 import sqlite3
+import os
 from datetime import datetime
+
+# Everything is accessible via the st.secrets dict:
+st.write("DB username:", st.secrets["db_username"])
+st.write("DB password:", st.secrets["db_password"])
+st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+# And the root-level secrets are also accessible as environment variables:
+st.write(
+    "Has environment variables been set:",
+    os.environ["db_username"] == st.secrets["db_username"],
+)
 
 # Connect to SQLite database
 conn = sqlite3.connect('inventory.db')
